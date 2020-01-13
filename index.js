31615cab43fbc4fac93cde9f63f2518ee65223aa
Roman <buffollSC@gmail.com>
2020-01-13 16:32:01 +0300

    Login

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const myToken = '995308759:AAG0cSOOdlAP8r3n6tnaXtBx0wArse89YDA';
 const port = "5010";
 const dataBase = "postgres://lfodygkpkxwiut:97c34dfaa384d8fae43c0ad8db1e3acec41a5ba9eb618eb74557116f2e5b8dbf@ec2-54-228-246-214.eu-west-1.compute.amazonaws.com:5432/ddf4globq3eaio";
+const applicationURL = 'https://heroku-telegram-bots.herokuapp.com:443';
+
 const Telegraf = require('telegraf');
 const Composer = require('telegraf/composer');
 const session = require('telegraf/session');
-const stage = require('telegraf/stage');
+const Stage = require('telegraf/stage');
 const Markup = require('telegraf/markup');
 const WizardScene = require('telegraf/scenes/wizard');
-const { Client } = require('pg');
 const extra = require('telegraf/extra');
-const applicationURL = 'https://heroku-telegram-bots.herokuapp.com:443';
+
+const { Client } = require('pg');
 
 const arrCard = [];
 const arrDate = [];
@@ -25,17 +27,18 @@ let client = new Client({
 });
   
 const stepHandler = new Composer();
-stepHandler.action('balance', async (ctx) => {
-    let userId = ctx.scene.session.state.result[0]
-    const resultId = await getBalance(userId)
-    ctx.reply(`Текущий баланс: $ ${resultId}. Для продолжения напишите что-нибудь`)
-    return ctx.scene.leave()
-})
+
+// stepHandler.action('balance', async (ctx) => {
+//     let userId = ctx.scene.session.state.result[0]
+//     const resultId = await getBalance(userId)
+//     ctx.reply(`Текущий баланс: $ ${resultId}. Для продолжения напишите что-нибудь`)
+//     return ctx.scene.leave()
+// })
   
-stepHandler.action('createCard', (ctx) => {
-    ctx.reply(`На какой день хотите создать карточку?`, createExpenseCard)
-    return ctx.wizard.next()
-})
+// stepHandler.action('createCard', (ctx) => {
+//     ctx.reply(`На какой день хотите создать карточку?`, createExpenseCard)
+//     return ctx.wizard.next()
+// })
   
 stepHandler.use((ctx) => ctx.replyWithMarkdown('Авторизация прошла успешно', successLogin));
 
@@ -73,95 +76,95 @@ const superWizard = new WizardScene('super-wizard',
   
     stepHandler,
   
-    (ctx) => {
+    // (ctx) => {
   
-      let callbackData = ctx.update.callback_query.data;
-      ctx.scene.session.state.result.push(callbackData);
+    //   let callbackData = ctx.update.callback_query.data;
+    //   ctx.scene.session.state.result.push(callbackData);
   
-      if (callbackData.toUpperCase() === 'CANCEL') {
-        ctx.reply('Авторизация прошла успешно', successLogin);
-        return ctx.wizard.back()
-      }
-      else if (callbackData.toUpperCase() === 'TODAY') {
-        ctx.reply('Что записывать в поле Amount?');
-        return ctx.wizard.selectStep(6)
-      }
-      else if (callbackData.toUpperCase() === 'CALENDAR') {
-        ctx.reply('Напишите дату в формате YYYY-MM-DD Например: 2012-11-28');
-        return ctx.wizard.next()
-      }
-    }, 
-    (ctx) => {
-      arrDate.push(ctx.message.text)
-      ctx.reply('Что записывать в поле Amount?');
-      return ctx.wizard.next()
-    }, 
-    (ctx) => {
-      arrCard.push(ctx.message.text)
-      ctx.reply('Что записывать в поле Description?');
-      return ctx.wizard.next()
-    }, 
-    (ctx) => {
-      arrCard.push(ctx.message.text)
-      let userId = ctx.scene.session.state.result[0];
-      let Amount = arrCard[0];
-      let Description = arrCard[1];
-      let cardDate = new Date().toUTCString();
+    //   if (callbackData.toUpperCase() === 'CANCEL') {
+    //     ctx.reply('Авторизация прошла успешно', successLogin);
+    //     return ctx.wizard.back()
+    //   }
+    //   else if (callbackData.toUpperCase() === 'TODAY') {
+    //     ctx.reply('Что записывать в поле Amount?');
+    //     return ctx.wizard.selectStep(6)
+    //   }
+    //   else if (callbackData.toUpperCase() === 'CALENDAR') {
+    //     ctx.reply('Напишите дату в формате YYYY-MM-DD Например: 2012-11-28');
+    //     return ctx.wizard.next()
+    //   }
+    // }, 
+    // (ctx) => {
+    //   arrDate.push(ctx.message.text)
+    //   ctx.reply('Что записывать в поле Amount?');
+    //   return ctx.wizard.next()
+    // }, 
+    // (ctx) => {
+    //   arrCard.push(ctx.message.text)
+    //   ctx.reply('Что записывать в поле Description?');
+    //   return ctx.wizard.next()
+    // }, 
+    // (ctx) => {
+    //   arrCard.push(ctx.message.text)
+    //   let userId = ctx.scene.session.state.result[0];
+    //   let Amount = arrCard[0];
+    //   let Description = arrCard[1];
+    //   let cardDate = new Date().toUTCString();
   
-      if (arrDate.length !== 0) {
-        cardDate = new Date(arrDate[0]).toUTCString();
-      }
-      setBalance(Amount, Description, userId, cardDate);
-      arrCard.length = 0;
-      arrDate.length = 0;
-      ctx.reply('Спасибо, запрос будет обработан.');
-      return ctx.scene.leave()
-    }
+    //   if (arrDate.length !== 0) {
+    //     cardDate = new Date(arrDate[0]).toUTCString();
+    //   }
+    //   setBalance(Amount, Description, userId, cardDate);
+    //   arrCard.length = 0;
+    //   arrDate.length = 0;
+    //   ctx.reply('Спасибо, запрос будет обработан.');
+    //   return ctx.scene.leave()
+    // }
   )
-  const getData = async(valueLogin, valuePass) => {
-    let tempArr = [];
-    const result = await client
-      .query(`SELECT sfid,email,password__c,office__c FROM salesforce.contact WHERE email = '${valueLogin}'
-      AND password__c = '${valuePass}';`)
+  // const getData = async(valueLogin, valuePass) => {
+  //   let tempArr = [];
+  //   const result = await client
+  //     .query(`SELECT sfid,email,password__c,office__c FROM salesforce.contact WHERE email = '${valueLogin}'
+  //     AND password__c = '${valuePass}';`)
 
-    for (let [keys, values] of Object.entries(result.rows)) {
-      for (let [key, value] of Object.entries(values)) {
-        tempArr.push(value);
-      }
-    }
-    if (!tempArr.length) {
-      tempArr.length = 0;
-    }
-    return tempArr;
-  };
-  const getBalance = async (valueId) => {
-    let tempArr = [];
-    const result = await client
-      .query(`SELECT sfid, Reminder__c, Keeper__c FROM salesforce.Monthly_Expense__c WHERE
-    Keeper__c = '${valueId}';`)
+  //   for (let [keys, values] of Object.entries(result.rows)) {
+  //     for (let [key, value] of Object.entries(values)) {
+  //       tempArr.push(value);
+  //     }
+  //   }
+  //   if (!tempArr.length) {
+  //     tempArr.length = 0;
+  //   }
+  //   return tempArr;
+  // };
+  // const getBalance = async (valueId) => {
+  //   let tempArr = [];
+  //   const result = await client
+  //     .query(`SELECT sfid, Reminder__c, Keeper__c FROM salesforce.Monthly_Expense__c WHERE
+  //   Keeper__c = '${valueId}';`)
 
-    for (let [keys, values] of Object.entries(result.rows)) {
-      for (let [key, value] of Object.entries(values)) {
-        if (key.toUpperCase() === 'REMINDER__C') {
-          tempArr.push(value);
-        }
-      }
-    }
-    if (!tempArr.length) {
-      totalAmount = 0;
-    }
-    const reducer = (accumulator, currentValue) => accumulator + currentValue;
-    var totalAmount = tempArr.reduce(reducer);
-    return totalAmount;
-  };
-  const setBalance = async (Amount, Description, userId, cardDate) => {
-    var parsedAmount = parseFloat(Amount, 10);
-    const MONTHLYFAKE = 'a012w000000VhXsAAK';
-    await client
-      .query(`INSERT INTO salesforce.Expense_Card__c
-      (Name, Amount__c, Card_Keeper__c, Card_Date__c,Description__c, Monthly_Expense__c, ExternalID__c)
-      VALUES('${userId}', ${parsedAmount}, '${userId}', '${cardDate}', '${Description}', '${MONTHLYFAKE}', gen_random_uuid());`)
-  };
+  //   for (let [keys, values] of Object.entries(result.rows)) {
+  //     for (let [key, value] of Object.entries(values)) {
+  //       if (key.toUpperCase() === 'REMINDER__C') {
+  //         tempArr.push(value);
+  //       }
+  //     }
+  //   }
+  //   if (!tempArr.length) {
+  //     totalAmount = 0;
+  //   }
+  //   const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  //   var totalAmount = tempArr.reduce(reducer);
+  //   return totalAmount;
+  // };
+  // const setBalance = async (Amount, Description, userId, cardDate) => {
+  //   var parsedAmount = parseFloat(Amount, 10);
+  //   const MONTHLYFAKE = 'a012w000000VhXsAAK';
+  //   await client
+  //     .query(`INSERT INTO salesforce.expense_card__c
+  //     (Name, Amount__c, Card_Keeper__c, Card_Date__c,Description__c, Monthly_Expense__c, ExterId__c)
+  //     VALUES('${userId}', ${parsedAmount}, '${userId}', '${cardDate}', '${Description}', '${MONTHLYFAKE}', gen_random_uuid());`)
+  // };
   client.connect();
   
   const bot = new Telegraf(API_TOKEN);
