b818619e4560d3846dbabd4b0d868bfe6cab7350
Roman <buffollSC@gmail.com>
2020-01-15 13:50:39 +0300

    chek creat card

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ const setBalance = async (Amount, Description, userId, cardDate) => {
   //const monthlyExpenseFake = 'a002w000001521UAKL';
   await client.query(`INSERT INTO salesforce.Expense_Card__c
   (Name, Amount__c, Card_Keeper__c, Card_Date__c, Description__c, ExterId__c)
-  VALUES('${userId}', ${Amount},'${userId}', '${cardDate}', '${Description}', gen_random_uuid());`)
+  VALUES('${userId}', ${Amount}, '${userId}', '${cardDate}', '${Description}', gen_random_uuid());`)
 };
 const superWizard = new WizardScene('super-wizard',
   (ctx) => {
@@ -145,11 +145,12 @@ const superWizard = new WizardScene('super-wizard',
     let userId = ctx.scene.session.state.allInformation[0];
     let Amount = arrCreatCard[0];
     let Description = arrCreatCard[1];
-    let cardDate = new Date(arrDate[0]).toUTCString();
+    let cardDate = new Date().toUTCString();
   
-    // if (arrDate.length !== 0) {
-    //   cardDate = new Date(arrDate[0]).toUTCString();
-    // }
+    if (arrDate.length !== 0) {
+      cardDate = new Date(arrDate[0]).toUTCString();
+    }
+    
     setBalance(Amount, Description, userId, cardDate);
     arrCreatCard.length = 0;
     arrDate.length = 0;
