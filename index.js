7fb9700aa6bcd8fb276082a6d0879e4918455dae
Roman <buffollSC@gmail.com>
2020-01-16 13:00:16 +0300

    fixed bugs

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Turned to the material : https://github.com/telegraf/telegraf/tree/develop/docs/examples
+// My git : https://github.com/buffollSC
 const myToken = '995308759:AAG0cSOOdlAP8r3n6tnaXtBx0wArse89YDA';
 const port = "5020";
 const dataBase = "postgres://lfodygkpkxwiut:97c34dfaa384d8fae43c0ad8db1e3acec41a5ba9eb618eb74557116f2e5b8dbf@ec2-54-228-246-214.eu-west-1.compute.amazonaws.com:5432/ddf4globq3eaio";
@@ -5,7 +7,6 @@ const applicationURL = 'https://heroku-telegram-bots.herokuapp.com';
 
 const Telegraf = require('telegraf');
 const telegrafStage = require('telegraf/stage');
-//const markup = require('telegraf/markup');
 const telegrafScenesWizard = require('telegraf/scenes/wizard');
 const extra = require('telegraf/extra');
 const { Client } = require('pg');
@@ -58,7 +59,7 @@ const getDataForAuthorization = async(valueLogin, valuePassword) => {
   let arrReturn = [];
   const allInformation = await client
     .query(`SELECT sfid, email, password__c, office__c 
-    FROM salesforce.contact 
+    FROM salesforce.Contact 
     WHERE email = '${valueLogin}'
     AND password__c = '${valuePassword}';`)
   for (let [keys, values] of Object.entries(allInformation.rows)) {
@@ -97,9 +98,6 @@ const setExpenseCard = async (Amount, Description, userId, cardDate) => {
   .query(`INSERT INTO salesforce.Expense_Card__c(Name, Amount__c, Card_Keeper__c, Card_Date__c, Description__c, ExterId__c)
   VALUES('${userId}', ${parsedAmount}, '${userId}', '${cardDate}', '${Description}', gen_random_uuid());`)
 };
-
-//stepForUser.use((ctx) => ctx.replyWithMarkdown('Авторизация прошла успешно', successMsg))
-
 //-----------------------Authorization----------------------------------
 const authorizationUser = new telegrafScenesWizard('authorization-User',
   (ctx) => {
@@ -118,7 +116,7 @@ const authorizationUser = new telegrafScenesWizard('authorization-User',
     Password = arrLoginAndPassword[1];
     const allInformation = await getDataForAuthorization(Login, Password)
     arrLoginAndPassword.length = 0;
-    if (allInformation.length === 4) {
+    if (allInformation === true) {
       ctx.scene.session.state = {
         allInformation : allInformation
       }
@@ -165,12 +163,10 @@ const authorizationUser = new telegrafScenesWizard('authorization-User',
     let Amount = arrInfaForExpCard[0];
     let Description = arrInfaForExpCard[1];
     let userId = ctx.scene.session.state.allInformation[0];
-    let cardDate = new Date().toUTCString();
-  
+    let cardDate;
     if (arrDateForExpCard.length != 0) {
       cardDate = new Date(arrDateForExpCard[0]).toUTCString();
     }
-
     setExpenseCard(Amount, Description, userId, cardDate);
     arrInfaForExpCard.length = 0;
     arrDateForExpCard.length = 0;
